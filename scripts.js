1b84cd39b1d2a2c9dc4cb58e4c3cd9b555bbebeb
Tajato <tahjgordon22@gmail.com>
2020-10-31 02:49:31 -0400

    updated js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,30 @@
 /* Add your JavaScript to this file */
+window.onload= function() {
+var email = document.getElementById('email');
+
+const form = document.getElementsByTagName('form')[0];
+const showError = document.getElementsByClassName('message')[0];
+var submitButton = document.getElementsByClassName('btn');
+
+
+form.addEventListener('submit', (e) => {
+
+if (email.value === "") {
+    e.preventDefault();
+    showError.innerHTML = "Please enter a valid email address";
+}
+
+else {
+    
+    showError.innerHTML = `Thank you! Your email ${email.value} has been added to our mailing list!`
+   
+   
+}
+    
+})
+
+
+
+}
+
+
